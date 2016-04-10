8ecc1f06e602c7a72bcf024085f21a35cea167d0
Shark <hoanghai92@hotmail.com>
2016-04-10 21:35:14 +0200

    design: add Heart icon

--- core/src/com/tdt4240grp8/game/screens/PlayScreen.java
+++ core/src/com/tdt4240grp8/game/screens/PlayScreen.java
@@ -11,6 +11,7 @@ import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.scenes.scene2d.ui.ProgressBar;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
@@ -32,8 +33,14 @@ public class PlayScreen implements Screen {
     private GoldWidget goldWidget1, goldWidget2;
     private HealthWidget healthWidget1, healthWidget2;
     private FreeTypeFontGenerator generator;
-    private FreeTypeFontGenerator.FreeTypeFontParameter parameter;
-    private BitmapFont font;
+    private FreeTypeFontGenerator.FreeTypeFontParameter goldFontParameter;
+    private FreeTypeFontGenerator.FreeTypeFontParameter healthFontParameter;
+
+    private BitmapFont goldFont;
+    private BitmapFont healthFont;
+
+    private ProgressBar progressBar1;
+    private ProgressBar.ProgressBarStyle progressBarStyle;
 
     public PlayScreen(GeometryAssault game) {
 
@@ -51,6 +58,9 @@ public class PlayScreen implements Screen {
         healthWidget1 = new HealthWidget(player1);
         healthWidget2 = new HealthWidget(player2);
 
+        progressBarStyle = new ProgressBar.ProgressBarStyle();
+        progressBar1 = new ProgressBar(10,100,10,false, progressBarStyle);
+
         player1.notifyAllObservers();
         player2.notifyAllObservers();
 
@@ -66,12 +76,19 @@ public class PlayScreen implements Screen {
         createButton(player2, "playbtn.png", 550, 50, st, Player.Fighters.TRIANGLE);
         createButton(player2, "playbtn.png", 650, 50, st, Player.Fighters.SQUARE);
 
-        createIcon("heart-icon.png", )
+        createIcon("heart-icon.png", 20 , GeometryAssault.HEIGHT - 100, st);
+        createIcon("heart-icon.png",GeometryAssault.WIDTH - 65, GeometryAssault.HEIGHT - 100, st);
+
 
         generator = new FreeTypeFontGenerator(Gdx.files.internal("fonts/Roboto-Bold.ttf"));
-        parameter = new FreeTypeFontGenerator.FreeTypeFontParameter();
-        parameter.size = 35;
-        font = generator.generateFont(parameter); // font size 12 pixels
+        goldFontParameter = new FreeTypeFontGenerator.FreeTypeFontParameter();
+        goldFontParameter.size = 35;
+        goldFont = generator.generateFont(goldFontParameter); // goldFont size 12 pixels
+
+        healthFontParameter = new FreeTypeFontGenerator.FreeTypeFontParameter();
+        healthFontParameter.size = 25;
+        healthFont = generator.generateFont(healthFontParameter);
+
         generator.dispose(); // don't forget to dispose to avoid memory leaks!
 
     }
@@ -92,6 +109,7 @@ public class PlayScreen implements Screen {
 
     private Image createIcon(String texturePath, int x, int y, Stage st){
         Image img = new Image(TextureManager.getInstance().getTexture(texturePath));
+        img.setSize(40,34);
         img.setPosition(x, y);
         st.addActor(img);
         return img;
@@ -143,7 +161,7 @@ public class PlayScreen implements Screen {
     @Override
     public void render(float delta) {
         update(delta);
-        Gdx.gl.glClearColor(1, 0, 0, 1);
+        Gdx.gl.glClearColor(0, 0, 0, 0);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         st.draw();
         game.batch.setProjectionMatrix(gamecam.combined);
@@ -158,12 +176,13 @@ public class PlayScreen implements Screen {
         }
 
         // Create goldwidget
-        font.setColor(Color.WHITE);
-        font.draw(game.batch, "$ " + goldWidget1.getGold(), 20, GeometryAssault.HEIGHT - 30);
-        font.draw(game.batch, "$ " + goldWidget2.getGold(), GeometryAssault.WIDTH -140, GeometryAssault.HEIGHT - 30);
-        font.draw(game.batch,  "HP: " + healthWidget1.getHealth(), 20 , GeometryAssault.HEIGHT - 70);
-        font.draw(game.batch, "HP: " + healthWidget2.getHealth(), GeometryAssault.WIDTH - 140, GeometryAssault.HEIGHT - 70);
-
+        goldFont.setColor(Color.WHITE);
+        goldFont.draw(game.batch, "$ " + goldWidget1.getGold(), 20, GeometryAssault.HEIGHT - 30);
+        goldFont.draw(game.batch, "$ " + goldWidget2.getGold(), GeometryAssault.WIDTH -140, GeometryAssault.HEIGHT - 30);
+        healthFont.draw(game.batch,  "" + healthWidget1.getHealth(), 70 , GeometryAssault.HEIGHT - 70);
+        healthFont.draw(game.batch, "" + healthWidget2.getHealth(), GeometryAssault.WIDTH - 100, GeometryAssault.HEIGHT - 70);
+//        progressBar1.draw(game.batch,1);
+//        st.addActor(progressBar1);
 
         game.batch.end();
     }
